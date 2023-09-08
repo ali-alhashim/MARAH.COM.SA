53d6f9f2acf5687a783cf81ac5d8d1b9df852053
ALI MUSA ALHASHIM <93006102+ali-alhashim@users.noreply.github.com>
2023-09-09 01:22:46 +0300

    mySQL

--- MARAH/settings.py
+++ MARAH/settings.py
@@ -9,13 +9,17 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-from firebase_admin import credentials
-import firebase_admin
+import configparser
 from pathlib import Path
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+configParser = configparser.RawConfigParser()
+configFilePath = os.path.join(BASE_DIR, 'config.cfg')
+print("the path file:", configFilePath)
+configParser.read(configFilePath)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -26,7 +30,7 @@ SECRET_KEY = 'django-insecure-0hxw8)+(pm20h-mb-w6($(gl1i*3&crt(5%spbj0zg0ama2@@!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -81,10 +85,18 @@ WSGI_APPLICATION = 'MARAH.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE'   : 'django.db.backends.mysql',
+        'NAME'     : configParser.get("DATABASE","NAME"),
+        'HOST'     : configParser.get("DATABASE","HOST"),
+        'USER'     : configParser.get("DATABASE","USER"),
+        'PASSWORD' : configParser.get("DATABASE","PASSWORD"),
+        'PORT'     : configParser.get("DATABASE","PORT"),
+        'OPTIONS'  : {
+                      'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+                     }
     }
 }
+
 
 
 # Password validation
