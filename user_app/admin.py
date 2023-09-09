598a98ec82f680cccc303dede96c63cae8dffb6a
ALI MUSA ALHASHIM <93006102+ali-alhashim@users.noreply.github.com>
2023-09-09 14:55:45 +0300

    admin & forms

--- user_app/admin.py
+++ user_app/admin.py
@@ -1,18 +1,42 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from .forms import CustomUserChangeForm
 from .models import User
+from .forms import UserChangeForm, UserCreationForm
 # Register your models here.
+from django.contrib.auth.forms import AdminPasswordChangeForm
 
 
+class UserAdmin(BaseUserAdmin):
 
-class UserAdmin(admin.ModelAdmin):
-    form = CustomUserChangeForm
-    list_display = ['full_name', 'email', 'mobile','last_login', 'email_verified', 'mobile_verified','nikname']
+    
+    list_display  = ['full_name', 'email', 'mobile','last_login', 'email_verified', 'mobile_verified','nikname']
     search_fields = ['full_name','email','mobile']
-    ordering = ['email']
-    exclude = ('date_joined','first_name','username','last_name')
+    ordering      = ['email',]
+    exclude       = ('date_joined','first_name','username','last_name')
+    change_password_form = AdminPasswordChangeForm
 
+    form      = UserChangeForm
+    add_form  = UserCreationForm
+    fieldsets = (
+    ('login info | معلومات الدخول', {'fields': ('email', 'password', 'mobile')}),
+    ('Personal info | المعلومات الشخصية', {'fields': ('full_name',)}),
+    ('Permissions | الصلاحيات', {'fields': ('is_admin', 'is_superuser', 'is_staff', 'is_active', 'groups')}),
+)
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+                        ('login info | معلومات الدخول', {
+                                                            'classes': ('wide',),
+                                                            'fields': ('email', 'password1', 'password2'),
+                                                         }
+                        ),
+
+                        ('Personal info | المعلومات الشخصية', {'fields': ('full_name', 'nikname')}),
+                        
+                        ('Permissions | الصلاحيات',   {'fields': ('is_admin', 'is_superuser','is_staff','is_active','groups')}),
+                    )
+
+    ordering = ['-date_joined',]
 admin.site.register(User, UserAdmin)
 
 
--- user_app/forms.py
+++ user_app/forms.py
@@ -51,4 +51,101 @@ class RegistrationForm(forms.ModelForm):
 
             if password != confirm_password:
                 raise forms.ValidationError('password dose not match !!')
-        
+
+
+
+class UserCreationForm(forms.ModelForm):
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput(attrs={'class':'form-control'}))
+
+    class Meta:
+        model = User
+        fields =  ('full_name','email', 'mobile', 'is_active','is_admin','is_staff','is_superuser', 'groups', 'user_permissions',)
+        widgets = {
+            'email':forms.TextInput(attrs={'class':'form-control'}),
+            'full_name':forms.TextInput(attrs={'class':'form-control'}),
+          
+               'groups':forms.SelectMultiple(attrs={'class':'form-select'}),
+                'user_permissions':forms.SelectMultiple(attrs={'class':'form-select'}),
+             
+        }            
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+    
+
+
+class UserChangeForm(forms.ModelForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    password hash display field.
+    """
+    password = ReadOnlyPasswordHashField(label= ("Password"))
+     
+
+    class Meta:
+        model = User
+        fields =  ('full_name','email','password', 'mobile','is_active','is_admin','is_staff','is_superuser',  'groups', 'user_permissions')
+        widgets = {
+            'email':forms.TextInput(attrs={'class':'form-control'}),
+            'full_name':forms.TextInput(attrs={'class':'form-control'}),
+            
+          
+             'mobile':forms.TextInput(attrs={'class':'form-control'}),
+         
+          
+              'groups':forms.SelectMultiple(attrs={'class':'form-select'}),
+                'user_permissions':forms.SelectMultiple(attrs={'class':'form-select'}),
+              
+              
+             
+        }            
+
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial["password"]
+
+class PasswordChangeForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput(attrs={'class':'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ('full_name','password')
+        widgets = {
+            'password':forms.TextInput(attrs={'class':'form-control', 'readonly':'True'}),
+            'full_name':forms.TextInput(attrs={'class':'form-control', 'readonly':'True'}),
+         }
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(PasswordChangeForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
